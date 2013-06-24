56344c1cb3a5e4f92f55d27e5267aea4f04fbae9
Jeremy Muhlich <jmuhlich@bitflood.org>
2013-06-24 12:06:40 -0400

    pull some drug info from drugbank file

--- parse_drugbank.py
+++ parse_drugbank.py
@@ -1,3 +1,4 @@
+import collections
 import lxml.etree
 
 ns = 'http://drugbank.ca'
@@ -5,13 +6,23 @@ ns = 'http://drugbank.ca'
 def qname(tag):
     return lxml.etree.QName(ns, tag).text
 
-def xpath(obj, path):
-    return obj.xpath(path, namespaces={'d': ns})
+def xpath(obj, path, single=True):
+    result = obj.xpath(path, namespaces={'d': ns})
+    if single:
+        if len(result) == 0:
+            result = None
+        elif len(result) == 1:
+            result = result[0]
+        else:
+            raise ValueError("XPath expression matches more than one value")
+    return result
 
 datafile_name = 'drugbank.xml'
 datafile = open(datafile_name)
 
 drug_targets = {}
+drug_info = []
+pubchem_cids = []
 partner_to_uniprot = {}
 
 for event, element in lxml.etree.iterparse(datafile):
@@ -19,23 +30,43 @@ for event, element in lxml.etree.iterparse(datafile):
     # unfortunate they re-used this tag name.
     if element.tag == qname('drug') and \
            element.getparent().tag != qname('drug-interaction'):
-        drugbank_id = xpath(element, 'd:drugbank-id/text()')[0]
-        cas_numbers = xpath(element, 'd:cas-number/text()')
-        assert len(cas_numbers) <= 1
-        partner_ids = xpath(element, 'd:targets/d:target/@partner')
-        if cas_numbers:
-            drug_targets[cas_numbers[0]] = partner_ids
+        drugbank_id = xpath(element, 'd:drugbank-id/text()')
+        name = xpath(element, 'd:name/text()')
+        synonyms = xpath(element, 'd:synonyms/d:synonym/text()', single=False)
+        molecular_formula = xpath(element, './/d:property'
+                                  '[d:kind="Molecular Formula"]/d:value/text()')
+        kegg_drug_id = xpath(element, './/d:external-identifier'
+                             '[d:resource="KEGG Drug"]/d:identifier/text()')
+        pubchem_cid = xpath(element, './/d:external-identifier'
+                            '[d:resource="PubChem Compound"]/d:identifier/text()')
+        partner_ids = xpath(element, 'd:targets/d:target/@partner', single=False)
+        if kegg_drug_id:
+            drug_targets[kegg_drug_id] = partner_ids
+        drug_info.append(collections.OrderedDict((
+                    ('name', name),
+                    ('synonyms', ';'.join(synonyms)),
+                    ('cid', pubchem_cid),
+                    ('mf', molecular_formula),
+                    )))
         element.clear()
     elif element.tag == qname('partner'):
         partner_id = element.get('id')
-        uniprot_ids = xpath(element, './/d:external-identifier'
-                            '[d:resource="UniProtKB"]/d:identifier/text()')
-        assert len(uniprot_ids) <= 1
-        partner_to_uniprot[partner_id] = uniprot_ids[0] if uniprot_ids else None
+        uniprot_id = xpath(element, './/d:external-identifier'
+                           '[d:resource="UniProtKB"]/d:identifier/text()')
+        partner_to_uniprot[partner_id] = uniprot_id
         element.clear()
 
 for drugbank_id, partner_ids in drug_targets.items():
     partner_ids[:] = [partner_to_uniprot[i] for i in partner_ids]
     partner_ids[:] = [i for i in partner_ids if i is not None]
-    
+
+
+# Targets -- kegg_drug_id: [uniprot_ids]
 print '\n'.join(map(str, sorted(drug_targets.items())))
+
+
+def to_utf8(v):
+    return encodings.utf_8.encode(v)[0] if v else ''
+drug_info_good_mf = [x for x in drug_info if x['mf'] and ' ' not in x['mf']]
+# Drugs -- name, synonyms, cid, mf
+#print '\n'.join('\t'.join(map(to_utf8, x.values())) for x in drug_info_good_mf)
