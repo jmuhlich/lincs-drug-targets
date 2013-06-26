3b177ccf973646daa8149ba579df698963b1f4e1
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2013-06-26 12:01:48 -0400

    10001 -> HMSL10001, etc.

--- extract_names.sh
+++ extract_names.sh
@@ -1,2 +1,3 @@
-
-tail +2 $1 | cutt 1-3 | pflane 'print "$F[0]\t$_" for grep /\S/, split(/\s*[,;]\s*/, "$F[1];$F[2]")' | gsort -u
+tail +2 $1 | cutt 1-3 | \
+  pflane 'print "HMSL$F[0]\t$_" for grep /\S/, split(/\s*[,;]\s*/, "$F[1];$F[2]")' | \
+  gsort -u
