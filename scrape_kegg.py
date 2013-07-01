1b267ca7e36c78423fc807c2b8722d8fb1415e4d
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2013-07-01 17:52:26 -0400

    implement compound scraping

--- scrape_kegg.py
+++ scrape_kegg.py
@@ -17,23 +17,48 @@ for line in fi.input():
         drugs.append(drug)
     drug2id[drug].add(id_)
 
-url_template = ('http://www.kegg.jp/kegg-bin/search?'
-                'q=%s&display=drug&from=drug&drug=&lang=en')
+
 
 def extract_id(a):
-    m = re.search(ur'\?dr:(\w+)$', a['href'])
+    m = re.search(ur'\?(?:dr|cpd):(\w+)$', a['href'])
     if not m:
         raise ValueError(a['href'])
     return m.group(1)
 
 
-for d in drugs:
-    html = ul2.urlopen(url_template % ul.quote(d)).read()
+def scrape_drug(kwd,
+                url_template=('http://www.kegg.jp/kegg-bin/search?'
+                              'q=%s&display=drug&from=drug&drug=&lang=en')):
+    html = ul2.urlopen(url_template % ul.quote(kwd)).read()
     if 'No data found' in html:
         ids = []
     else:
         soup = bs(html, 'lxml')
         ids = [extract_id(a) for a in soup.findAll('a') if 'dr:' in a['href']]
+
+    return ids
+
+
+def scrape_compound(kwd,
+                    url_template=('http://www.genome.jp/'
+                                  'dbget-bin/www_bfind_sub?'
+                                  'mode=bfind&max_hit=1000&'
+                                  'dbkey=compound&keywords=%s')):
+    html = ul2.urlopen(url_template % ul.quote(kwd)).read()
+    if 'NO ENTRY FOUND' in html:
+        ids = []
+    else:
+        soup = bs(html, 'lxml')
+        ids = [extract_id(a) for a in soup.findAll('a') if 'cpd:' in a['href']]
+
+    return ids
+
+for d in drugs:
+
+    ids = scrape_drug(d)
+
+    if not ids:
+        ids = scrape_compound(d)
 
     kegg_drug_ids = '|'.join(sorted(ids))
 
