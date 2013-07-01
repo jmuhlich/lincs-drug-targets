b9f66aee6438861e74fc3eefad13c9fef4cba289
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2013-07-01 17:53:14 -0400

    minor refactoring

--- process_scraped_kegg.py
+++ process_scraped_kegg.py
@@ -3,9 +3,6 @@ import re
 import fileinput as fi
 import kegg_utils as ku
 
-recs = [t for t in tuple(csv.reader(fi.input(), delimiter='\t'))
-        if t[-1] != '']
-
 def scrub(s, _gunk=re.compile(ur'(?:\s+\(.*?\))+$')):
     return _gunk.sub('', s.strip('; '))
 
@@ -31,27 +28,43 @@ def check(kegg_names, hmsl_names):
     checked = [k for k in kegg_names if match(k, hmsl_names)]
     return len(checked) > 0
 
-collect = dict()
+def collect_data(input_handle):
+    recs = tuple(csv.reader(input_handle, delimiter='\t'))
 
-for r in recs:
-    hmsl, name, kegg = r
-    hmsl = hmsl[:-4]
-    kegg = kegg.split('|')
-    c = collect.setdefault(hmsl, [set(), set()])
-    c[0].add(name)
-    c[1].update(set(kegg))
+    collect = dict()
 
-tocheck = dict()
-for hsml, c in collect.items():
-    hmsl_names, kegg_drug_ids = c
-    kegg_drug_names = [(k, get_kegg_drug_names(k)) for k in kegg_drug_ids]
-    c[1] = [('' if check(kns, hmsl_names) else 'x', k, kns)
-            for k, kns in kegg_drug_names]
-    c[0] = ' ; '.join(hmsl_names)
+    for r in recs:
+        hmsl, name, kegg = r
+        hmsl = hmsl[:-4]
+        c = collect.setdefault(hmsl, [set(), set()])
+        c[0].add(name)
+        if len(kegg):
+            kegg = kegg.split('|')
+            c[1].update(set(kegg))
 
-for hmsl in sorted(collect.keys()):
-    names, kegg_data = collect[hmsl]
-    for kd in sorted(kegg_data, key=lambda i: (i[0], i[1])):
-        mark, kegg_id, kegg_names = kd
-        print '\t'.join([mark, hmsl, names, kegg_id,
-                         ' ; '.join([n.strip(' ;') for n in kegg_names])])
+    tocheck = dict()
+    for c in collect.values():
+        hmsl_names, kegg_drug_ids = c
+        kegg_drug_names = [(k, get_kegg_drug_names(k)) for k in kegg_drug_ids]
+        c[1] = [('' if check(kns, hmsl_names) else 'x', k, kns)
+                for k, kns in kegg_drug_names]
+        c[0] = ' ; '.join(hmsl_names)
+
+    return collect
+
+
+def print_output(collect):
+    for hmsl in sorted(collect.keys()):
+        names, kegg_data = collect[hmsl]
+        recs = sorted(kegg_data, key=lambda i: (i[0], i[1]))
+
+        if not recs:
+            recs = [('', '', [''])]
+
+        for kd in recs:
+            mark, kegg_id, kegg_names = kd
+            print '\t'.join([mark, hmsl, names, kegg_id,
+                             ' ; '.join([n.strip(' ;') for n in kegg_names])])
+
+if __name__ == '__main__':
+    print_output(collect_data(fi.input()))
