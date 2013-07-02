a87652f9ca78cbbbf9b14a338c1949dbf2aa86e4
Jeremy Muhlich <jmuhlich@bitflood.org>
2013-07-02 13:07:13 -0400

    more sqlalchemy work

--- parse_drugbank.py
+++ parse_drugbank.py
@@ -18,23 +18,25 @@ def xpath(obj, path, single=True):
 
 engine = sa.create_engine('sqlite:///:memory:')
 metadata = sa.MetaData(bind=engine)
-drugbank_drugs = sa.Table(
-    'drugbank_drugs', metadata,
-    sa.Column('drugbank_id', sa.String(), primary_key=True),
+drugbank_drug = sa.Table(
+    'drugbank_drug', metadata,
+    sa.Column('drug_id', sa.String(), primary_key=True),
     sa.Column('name', sa.String()),
-    sa.Column('synonyms', sa.String()),
+    sa.Column('synonyms', sa.PickleType()),  # list of strings
     sa.Column('kegg_id', sa.String()),
-    sa.Column('cid', sa.String()),
-    sa.Column('mf', sa.String()),
+    sa.Column('pubchem_cid', sa.String()),
+    sa.Column('molecular_formula', sa.String()),
+    sa.Column('partners', sa.PickleType()),  # list of strings
+    )
+hmsl_name = sa.Table(
+    'hmsl_name', metadata,
+    sa.Column('name', sa.String(), primary_key=True),
+    sa.Column('hmsl_id', sa.String()),
     )
 metadata.create_all()
 
 datafile_name = 'drugbank.xml'
 datafile = open(datafile_name)
-
-drug_targets = {}
-pubchem_cids = []
-partner_to_uniprot = {}
 
 ns = 'http://drugbank.ca'
 
@@ -46,20 +48,21 @@ for event, element in lxml.etree.iterparse(datafile, tag=qnames['drug']):
     # unfortunate they re-used this tag name.
     if element.getparent().tag == qnames['drug-interaction']:
         continue
-    drugbank_id = xpath(element, 'd:drugbank-id/text()')
+    drug_id = xpath(element, 'd:drugbank-id/text()')
     name = xpath(element, 'd:name/text()')
     synonyms = xpath(element, 'd:synonyms/d:synonym/text()', single=False)
     molecular_formula = xpath(element, './/d:property'
                               '[d:kind="Molecular Formula"]/d:value/text()')
-    kegg_drug_id = xpath(element, './/d:external-identifier'
-                         '[d:resource="KEGG Drug"]/d:identifier/text()')
+    kegg_id = xpath(element, './/d:external-identifier'
+                    '[d:resource="KEGG Drug"]/d:identifier/text()')
     pubchem_cid = xpath(element, './/d:external-identifier'
                         '[d:resource="PubChem Compound"]/d:identifier/text()')
     partner_ids = xpath(element, 'd:targets/d:target/@partner', single=False)
-    drug_targets[drugbank_id] = partner_ids
-    engine.execute(drugbank_drugs.insert().values(
-            drugbank_id=drugbank_id, name=name, synonyms=';'.join(synonyms),
-            kegg_id=kegg_drug_id, cid=pubchem_cid, mf=molecular_formula))
+    engine.execute(drugbank_drug.insert().
+                   values(drug_id=drug_id, name=name, synonyms=synonyms,
+                          kegg_id=kegg_id, pubchem_cid=pubchem_cid,
+                          molecular_formula=molecular_formula,
+                          partners=partner_ids))
     element.clear()
 
 # Turns out it's much faster to do a second iterparse loop with a different
@@ -68,6 +71,7 @@ for event, element in lxml.etree.iterparse(datafile, tag=qnames['drug']):
 # than we are, and the disk I/O and buffer cache impact are negligible. It
 # would be nice if the tag argument could accept a list of tag names...
 datafile.seek(0)
+partner_to_uniprot = {}
 for event, element in lxml.etree.iterparse(datafile, tag=qnames['partner']):
     partner_id = element.get('id')
     uniprot_id = xpath(element, './/d:external-identifier'
@@ -75,9 +79,16 @@ for event, element in lxml.etree.iterparse(datafile, tag=qnames['partner']):
     partner_to_uniprot[partner_id] = uniprot_id
     element.clear()
 
-for drugbank_id, partner_ids in drug_targets.items():
-    partner_ids[:] = [partner_to_uniprot[i] for i in partner_ids]
-    partner_ids[:] = [i for i in partner_ids if i is not None]
+# Use a transaction to avoid re-reading our own updates (partners would already
+# be updated).
+with engine.begin() as connection:
+    for rec in connection.execute(drugbank_drug.select()):
+        new_values = dict(rec)
+        new_values['partners'] = map(partner_to_uniprot.__getitem__, rec.partners)
+        new_values['partners'] = filter(None, new_values['partners'])
+        connection.execute(drugbank_drug.update().
+                           where(drugbank_drug.c.drug_id == rec.drug_id).
+                           values(**new_values))
 
 # Targets -- kegg_drug_id: [uniprot_ids]
 #print '\n'.join(map(str, sorted(drug_targets.items())))
@@ -96,20 +107,22 @@ sm_names_file = open(sm_names_filename, 'rb')
 sm_names_reader = csv.DictReader(sm_names_file, fieldnames=('hmsl_id', 'name'),
                                  dialect='excel-tab')
 all_names = []
-name_to_hmsl = {}
-cid_to_hmsl = {}
 found_conflicts = False
 for row in sm_names_reader:
-    hmsl_id = row['hmsl_id']
-    hmsl_id = hmsl_id[:5]
-    name = row['name']
-    all_names.append(name)
-    if name in name_to_hmsl and name_to_hmsl[name] != hmsl_id:
-        print "CONFLICT: %s - %s / %s" % (name, name_to_hmsl[name], hmsl_id)
-        found_conflicts = True
-    name_to_hmsl[name] = hmsl_id
+    row['hmsl_id'] = row['hmsl_id'][:5]
+    all_names.append(row['name'])
+    try:
+        engine.execute(hmsl_name.insert().values(**row))
+    except sa.exc.IntegrityError as e:
+        r = engine.execute(hmsl_name.select().
+                           where(hmsl_name.c.name == row['name']))
+        rec = r.first()
+        if rec and rec.hmsl_id != row['hmsl_id']:
+            print "CONFLICT: %s - %s / %s" % (row['name'],
+                                              rec.hmsl_id, row['hmsl_id'])
+            found_conflicts = True
 if found_conflicts:
-    exit()
+    sys.exit()
 
 sm_filename = os.path.join(os.path.dirname(sys.argv[0]),
                            'small_molecule.130625T133836.tsv')
@@ -125,12 +138,12 @@ for row in sm_reader:
     mf_to_hmsl[row['Molecular Formula']] = hmsl_id
 
 drugbank_to_hmsl = {}
-for di in engine.execute(drugbank_drugs.select()):
-    cid_match_id = cid_to_hmsl.get(di.cid)
+for di in engine.execute(drugbank_drug.select()):
+    cid_match_id = cid_to_hmsl.get(di.pubchem_cid)
     if cid_match_id:
-        drugbank_to_hmsl[di.drugbank_id] = \
+        drugbank_to_hmsl[di.drug_id] = \
             [(cid_match_id, 'CID match: %s (%s)' %
-              (di.cid, di.name))]
+              (di.pubchem_cid, di.name))]
     else:
         name_matches = difflib.get_close_matches(di.name, all_names,
                                                  cutoff=0.8)
@@ -138,7 +151,7 @@ for di in engine.execute(drugbank_drugs.select()):
         if name_matches:
 #            for name in name_matches:
 #                hmsl_record = sm_data[name_to_hmsl[n]]
-            drugbank_to_hmsl[di.drugbank_id] = \
+            drugbank_to_hmsl[di.drug_id] = \
                 [(name_to_hmsl[n], 'Name match: %s -> %s' % (di.name, n))
                  for n in name_matches]
 
